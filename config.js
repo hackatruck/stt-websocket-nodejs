1e9c855497ff65af6cc7b765fc9ba29709a292f4
Leandro David <leandrodvd@gmail.com>
2017-07-11 11:52:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 var config = {};
 
+
 // Find how to get a token at:
 // https://www.ibm.com/watson/developercloud/doc/common/getting-started-tokens.html
 config.token = "";
-
+config.username = "";
+config.password = ""
+config.keywords=[];
+config.smart_formatting=true;
+config.profanity_filter=true;
+config.inactivity_timeout=-1;
+config.interim_results=true;
 // Some valid voices to choose
 // ar-AR_BroadbandModel
 // en-UK_BroadbandModel
@@ -19,8 +26,6 @@ config.token = "";
 // pt-BR_NarrowbandModel
 // zh-CN_BroadbandModel
 // zh-CN_NarrowbandModel
-
-
 config.model="pt-BR_BroadbandModel";
 
 module.exports = config;
