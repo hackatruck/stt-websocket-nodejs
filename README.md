4053607f74e927b9adefe27d219c74a93ec74a03
Leandro David <leandrodvd@gmail.com>
2017-07-11 11:48:23 -0300

    update app

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # stt-websocket-nodejs
+Projeto exemplo em NodeJS usando Watson Speech to Text para transcrever o audio do microfone usando web sockets
+
+# Instalação e configuração
+
+## Clone e instale as dependencias
+```
+git clone https://github.com/hackatruck/stt-websocket-nodejs.git
+cd stt-websocket-nodejs
+npm install
+```
+
+## Configure as credenciais do serviço Watson Conversation
+
+1. Acesse [bluemix.net](bluemix.net)
+2. Crie uma instância do serviço Watson Speech to Text
+3. Copie as credenciais do serviço (username e password) para o arquivo config.js
+
+# Execute a aplicação
+Execute a aplicação de exemplo que utiliza a biblioteca watson-developer-cloud com o comando
+
+
+    node app.js
+
+
+Execute a aplicação de exemplo que utiliza websockets puros, sem o uso da biblioteca watson-developer-cloud com o comando
+
+    node app_ws.js
--- app.js
+++ app.js
@@ -1,37 +1,68 @@
-var config = require('./config');
-var WebSocket = require('ws');
-var fs = require('fs');
+var watson = require('watson-developer-cloud'); //to connect to Watson developer cloud
+var mic = require('mic');
+var config = require("./config.js") // to get our credentials and the attention word from the config.js files
 
-var wsURI = "wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize"
-  + "?watson-token=" + config.token + '&model=' + config.model;
 
-var websocket = new WebSocket(wsURI);
-websocket.onopen = function(evt) { onOpen(evt) };
-websocket.onclose = function(evt) { onClose(evt) };
-websocket.onmessage = function(evt) { onMessage(evt) };
-websocket.onerror = function(evt) { onError(evt) };
+var speech_to_text = watson.speech_to_text({
+  username: config.username,
+  password: config.password,
+  version: 'v1'
+});
 
-function onOpen(evt) {
-  var message = {
-    action: 'start',
-    'content-type': 'audio/wav',
-    'interim_results': true,
-    'max-alternatives': 3,
-    'keywords_threshold': 0.5
-  };
-  websocket.send(JSON.stringify(message));
 
-  // Prepare and send the audio file.
-  var file = fs.readFileSync(__dirname+"/test.wav");
-  websocket.send(file);
+// Initiate Microphone Instance to Get audio samples
+console.log("Configurando microfone");
+var micInstance = mic({ 'rate': '44100', 'channels': '2', 'debug': false, 'exitOnSilence': 6 });
+var micInputStream = micInstance.getAudioStream();
 
-  websocket.send(JSON.stringify({action: 'stop'}));
+micInputStream.on('data', function(data) {
+  // console.log("Recieved Input Stream: " + data.length);
+});
+
+micInputStream.on('error', function(err) {
+  console.log("Error in Input Stream: " + err);
+});
+
+micInputStream.on('silence', function() {
+  // detect silence.
+});
+
+micInstance.start();
+console.log("Microfone ligado");
+
+
+function initTextStream(){
+  console.log("init text stream")
+  textStream = micInputStream.pipe(speech_to_text.createRecognizeStream({
+    content_type: 'audio/l16; rate=44100; channels=2',
+    model: config.model,
+    customization_id: config.STTCustomizationID,
+    interim_results: config.interim_results,
+    inactivity_timeout: config.inactivity_timeout,
+    profanity_filter: config.profanity_filter,
+    keywords: config.keywords,
+    smart_formatting: config.smart_formatting
+  }));
+
+  textStream.setEncoding('utf8');
+
+  var context = {} ; // Save information on conversation context/stage for continous conversation
+  textStream.setEncoding('utf8');
+
+  textStream.on('close', function(event) {
+    console.log("Speech to Text Stream closed");
+    console.log(JSON.stringify(event, null, 2));
+    initTextStream();
+  });
+
+  textStream.on('data', function(str) {
+    console.log(str); // print the text once received
+  });
+
+  textStream.on('error', function(err) {
+    console.log(' === Watson Speech to Text : An Error has occurred =====') ; // handle errors
+    console.log(err) ;
+    console.log("Press <ctrl>+C to exit.") ;
+  });
 }
-
-function onMessage(evt) {
-  console.log(evt.data);
-}
-
-function onDlose(evt) {
-  console.log("websocket closed");
-}
+initTextStream();
